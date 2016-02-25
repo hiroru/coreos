454bba147bdc1bd861f3acf3a92d81d4dca7c4dd
Sergi Barroso <hiroru@lionclan.org>
2016-02-25 16:30:43 +0100

    Added a dirty script to fix cloud-init info during boot of CoreOS

---
+++ cloud-init-fix.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Dirty fix for CoreOS cloud-init on Openstack with multiple interfaces.
+# by Sergi Barroso <hiroru@lionclan.org>
+
+if ! [ -f /etc/environment ]; then
+   if [ -z "$(mount | awk '/oem/ && /rw/ {print}')" ]; then
+      sudo mount -o remount,rw /usr/share/oem/
+   fi
+   sudo echo "COREOS_PUBLIC_IPV4=$(ip addr | grep eth1 | sed -n 's/.*inet.\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\).*/\1/p')" > /etc/environment
+   sudo echo "COREOS_PRIVATE_IPV4=$(ip addr | grep eth0 | sed -n 's/.*inet.\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\).*/\1/p')" >> /etc/environment
+   sudo sed -i 's/--oem=ec2-compat/--from-configdrive=\/media\/configdrive\//g' /usr/share/oem/cloud-config.yml
+   sudo /usr/bin/coreos-cloudinit --from-configdrive=/media/configdrive/
+   sudo reboot
+fi
