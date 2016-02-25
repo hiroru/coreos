5fab2bfed9f435b147163d6d632eb919eb329be8
Sergi Barroso <hiroru@lionclan.org>
2016-02-25 16:55:38 +0100

    script: echoing variable into environment file

--- cloud-init-fix.sh
+++ cloud-init-fix.sh
@@ -8,6 +8,8 @@ if ! [ -f /etc/environment ]; then
    fi
    sudo echo "COREOS_PUBLIC_IPV4=$(ip addr | grep eth1 | sed -n 's/.*inet.\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\).*/\1/p')" > /etc/environment
    sudo echo "COREOS_PRIVATE_IPV4=$(ip addr | grep eth0 | sed -n 's/.*inet.\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\).*/\1/p')" >> /etc/environment
+   sudo echo "\$private_ipv4=$(ip addr | grep eth0 | sed -n 's/.*inet.\([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\).*/\1/p')" >> /etc/environment
+   source /etc/environment
    sudo sed -i 's/--oem=ec2-compat/--from-configdrive=\/media\/configdrive\//g' /usr/share/oem/cloud-config.yml
    sudo /usr/bin/coreos-cloudinit --from-configdrive=/media/configdrive/
    sudo reboot
